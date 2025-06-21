add70a04cbebae1cfeb5d154de5950c431568705
JC Icaro <jc.icaro@gmail.com>
2025-06-21 11:36:07 +0000

    adjust navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,36 @@ export default function Navbar() {
           CAEDN
         </Link>
       </div>
-      <div className="flex-none hidden md:flex space-x-4">
+
+
+      {/* <div className="flex-none hidden md:flex space-x-4">
         <Link to="/" className="btn btn-ghost">Home</Link>
-        {/* <Link to="/about" className="btn btn-ghost">About</Link> */}
         <Link to="/basicmath" className="btn btn-ghost">Basic Math</Link>
         <Link to="/multiplication" className="btn btn-ghost">Multiplication</Link>
-        {/* Add more links as needed */}
+      </div> */}
+
+      
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-20">
+          <li><Link to="/" className="btn btn-ghost">Home</Link></li>
+          <li>
+            <details>
+              <summary className="btn btn-ghost">Learn</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li>
+                  <Link to="/basicmath" className="btn btn-ghost">Basic Math</Link>
+                </li>
+                <li>
+                  <Link to="/multiplication" className="btn btn-ghost">Multiplication</Link>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
       </div>
+
+
+
       <div className="dropdown dropdown-end md:hidden">
         <label tabIndex={0} className="btn btn-ghost btn-circle">
           <svg
@@ -23,7 +46,7 @@ export default function Navbar() {
             stroke="currentColor"
             viewBox="0 0 24 24"
           >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </label>
         <ul
@@ -31,11 +54,12 @@ export default function Navbar() {
           className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-40"
         >
           <li><Link to="/">Home</Link></li>
-          {/* <li><Link to="/about">About</Link></li> */}
           <li><Link to="/basicmath">Basic Math</Link></li>
           <li><Link to="/multiplication">Multiplication</Link></li>
         </ul>
       </div>
+
+
     </div>
   );
 }
