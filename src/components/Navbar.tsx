f45bad8684ca380c95ee77357998dd5beb6d5e8d
JC Icaro <jc.icaro@gmail.com>
2025-06-21 22:54:49 +0000

    updated site name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div className="navbar bg-base-100 shadow-md px-4">
       <div className="flex-1">
         <Link to="/" className="text-xl font-bold text-black">
-          CAEDN
+          Luna Icaro
         </Link>
       </div>
 
