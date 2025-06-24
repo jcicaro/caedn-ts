5d2e5f2af93f9d1b63579ccd3e0b57ad9ab6e026
JC Icaro <jc.icaro@gmail.com>
2025-06-24 04:41:46 +0000

    add code highlighting support

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import rehypeHighlight from 'rehype-highlight';
 import { ChatMessage } from '../services/openaiService'
 
 export function ChatBubble({ msg }: { msg: ChatMessage }) {
@@ -19,7 +20,7 @@ export function ChatBubble({ msg }: { msg: ChatMessage }) {
         }`}
         style={{ whiteSpace: 'pre-wrap' }}
       >
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
           {msg.content}
         </ReactMarkdown>
       </div>
